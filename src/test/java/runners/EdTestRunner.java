041c6211bbdb7540980a298422ad613e90daed32
swapnakatnuru <84848806+swapnakatnuru@users.noreply.github.com>
2022-03-17 21:41:16 +0000

    123

--- src/test/java/runners/EdTestRunner.java
+++ src/test/java/runners/EdTestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/Features/Payment.feature"},
+        features = {"src/test/resources/Features/gitt.feature"},
          glue={"Steps1"},
         plugin = {"pretty","html:target/cucumber.html"}
         //tags = "@ui"
